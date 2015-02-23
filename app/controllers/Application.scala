084e34cdec83326cf8889be3e769470cfd139f3d
wongcy6 <wongcy6@hotmail.com>
2015-02-23 00:14:40 -0800

    Add support for Json endpoint; Add boilerplate for Backbone

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.main("MyTitle"))
+    Ok(views.html.main())
   }
 
+  def list = Action {
+    Ok(Json.toJson(Map("name" -> "Richard", "Gender" -> "Male")))
+  }
 }
