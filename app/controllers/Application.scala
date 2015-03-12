c4e671e8a203a8fcc7721f3e817a533dcf541d4e
wongcy6 <wongcy6@hotmail.com>
2015-03-11 23:01:06 -0700

    Add heatmap and other features

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,17 @@
 package controllers
 
+import java.util.Date
+
+import akka.actor.{Props, ActorRef}
 import play.api._
+import play.api.libs.concurrent.Promise
+import play.api.libs.iteratee.{Enumerator, Iteratee}
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.duration.Duration
+import scala.util.Random
 
 object Application extends Controller {
 
@@ -13,4 +22,33 @@ object Application extends Controller {
   def list = Action {
     Ok(Json.toJson(Map("name" -> "Isabella", "Gender" -> "Dog")))
   }
+
+  def fb = Action {
+    Ok(views.html.fb())
+  }
+
+  def react = Action {
+    Ok(views.html.react())
+  }
+
+  def tts = Action {
+    Ok(views.html.tts())
+  }
+
+  def heatmap = Action {
+    Ok(views.html.heatmap())
+  }
+
+  def ws = WebSocket.using[String] { request =>
+    val random = Random
+    val in = Iteratee.consume[String]()
+
+    val out = Enumerator.generateM [String] {
+      val ipString = "%d.%d.%d.%d".format(random.nextInt(255)+1, random.nextInt(255)+1, random.nextInt(255)+1, random.nextInt(255)+1)
+      Promise.timeout(Some(ipString), Duration(5000, "millis"))
+    }
+
+    (in, out)
+  }
+
 }
